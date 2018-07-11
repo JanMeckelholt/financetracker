c22df2e760fa56e7e40b6ed42a30e66c788a983c
JanMeckelholt <janmeckelholt@gmail.com>
2018-07-11 21:08:46 +0200

    ajax for stock lookup

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,18 @@
 class StocksController < ApplicationController
-  def search
-  	unless params[:stock].blank?
-  		@stock = Stock.new_from_lookup(params[:stock])
-  		if @stock == nil
-	  	  flash[:warning] = "Stock not found!"
-	  	 end
-	  	  
-  	end
+def search
+  if params[:stock].present?
+    @stock = Stock.new_from_lookup(params[:stock])
+    if @stock
+      respond_to do |format|
+        format.js { render partial: 'stocks/result' }
+      end
+    else
+      flash.now[:danger] = "You have entered an incorrect symbol"
+      redirect_to stocks_search_path
+    end
+  else
+    flash.now[:danger] = "You have entered an empty search string"
+    redirect_to stocks_search_path
   end
+end
 end
