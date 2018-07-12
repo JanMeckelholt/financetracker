63bb16bf45d92c480b061119da6369ae10b85ed6
JanMeckelholt <janmeckelholt@gmail.com>
2018-07-12 06:02:17 +0200

    ajax error msg - empty still missing

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,24 @@
 class StocksController < ApplicationController
-def search
-  if params[:stock].present?
-    @stock = Stock.new_from_lookup(params[:stock])
-    if @stock
-      respond_to do |format|
-        format.js { render partial: 'stocks/result' }
-      end
-    else
-      flash.now[:danger] = "You have entered an incorrect symbol"
-      redirect_to stocks_search_path
-    end
-  else
-    flash.now[:danger] = "You have entered an empty search string"
-    redirect_to stocks_search_path
-  end
-end
+	
+	def search
+	  if params[:stock].present?
+	    @stock = Stock.new_from_lookup(params[:stock])
+	    if @stock
+	      respond_to do |format|
+	        format.js { render partial: 'stocks/result' }
+	      end
+	    else
+	    	respond_to do |format|
+	        flash.now[:danger] = "You have entered an incorrect symbol2"
+	        format.js { render partial: 'stocks/result' }
+	  	  end
+	  	end
+	  #else
+	  #	respond_to do |format|
+	   # 	flash.now[:danger] = "You have entered an empty search string2"
+	    #	format.js {render  partial: 'stocks/result' }
+	  	#end
+		end
+	end
+
 end
