8a8fa5acc68212df9c9159e605b894870a7956c3
JanMeckelholt <janmeckelholt@gmail.com>
2018-07-12 20:58:53 +0200

    Ajax format fixed - html-added

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,19 +5,28 @@ class StocksController < ApplicationController
 	    @stock = Stock.new_from_lookup(params[:stock])
 	    if @stock
 	      respond_to do |format|
+	      	format.html
 	        format.js { render partial: 'stocks/result' }
 	      end
 	    else
 	    	respond_to do |format|
-	        flash.now[:danger] = "You have entered an incorrect symbol2"
-	        format.js { render partial: 'stocks/result' }
+	    		format.html
+	        
+	        format.js { 
+	        	flash.now[:danger] = "You have entered an incorrect symbol2"
+	        	render partial: 'stocks/result' 
+	        }
 	  	  end
 	  	end
-	  #else
-	  #	respond_to do |format|
-	   # 	flash.now[:danger] = "You have entered an empty search string2"
-	    #	format.js {render  partial: 'stocks/result' }
-	  	#end
+	  else
+	  	respond_to do |format|
+	  		format.html
+	    	
+	    	format.js {
+					flash.now[:danger] = "You have entered an empty search string2"
+	    		render  partial: 'stocks/result' 
+	    	}
+	  	end
 		end
 	end
 
