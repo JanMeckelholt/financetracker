56cc5fd8e7441df8fec9dd9e9b96e5e20302756a
JanMeckelholt <janmeckelholt@gmail.com>
2018-07-23 12:56:30 +0200

    building friendship functionaltiy - Add friends to be next

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,23 @@ class UsersController < ApplicationController
   end
 
   def my_friends
+  	@my_friends = current_user.friends
   end
+
+  def remove_friend
+		friend = User.where(id: params[:friend])
+		
+		user_friend = Friendship.where(user: current_user, friend: friend)
+
+		
+		
+		if user_friend.delete
+			flash[:success] = "Friendship deleted"
+			redirect_to my_friends_path
+		else
+			flash[:danger] = "Friendship could not be deleted."
+			redirect_to my_friends_path
+		end
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'my_friends', to: 'users#my_friends'
+  post 'users/remove_friend'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
